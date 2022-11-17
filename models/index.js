437c35b05ad5746c36820fdbf9cc545d1298b382
MKHLink <[likhonhasankomol@gmail.com]>
2022-11-16 20:59:38 -0500

    finished the models and entity relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'cascade'
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+through: ProductTag,
+as: 'tags',
+foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  as:'tags',
+  foreignKey:'tag_id'
+});
+
 
 module.exports = {
   Product,
